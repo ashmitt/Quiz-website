482d5184195ef0d0a625220bd314f9e909c69efb
ashmitt <ashmitrai0905@gmail.com>
2025-06-22 21:29:34 +0530

    fix: Relocate index.html to root and simplify Vite config

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root: '.',
-  publicDir: 'public',
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets'
-  }
 })
