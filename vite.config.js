5583e293d9a1955f3ddcba24aea4115c7073f7c4
ashmitt <ashmitrai0905@gmail.com>
2025-06-22 21:27:01 +0530

    fix(vite): Explicitly set root and publicDir for Netlify

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: '.',
+  publicDir: 'public',
   plugins: [react()],
-  server: {
-    host: '127.0.0.1',
-    port: 5173
-  } ,build: {
-    rollupOptions: {
-      input: 'index.html'
-    }
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
   }
 })
